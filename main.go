74bd0cdccd5694b22dfe76238895a051b782b62e
Monokai <2536818783@qq.com>
2018-08-07 11:12:41 +0800

    省去static/html路径

--- main.go
+++ main.go
@@ -3,9 +3,28 @@ package main
 import (
 	_ "myproject/loveHome/routers"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
+	"strings"
+	"net/http"
 )
 
+func ignoreStaticPath(){
+	beego.InsertFilter("/",beego.BeforeRouter,TransparentStatic)
+	beego.InsertFilter("/*",beego.BeforeRouter,TransparentStatic)
+}
+
+func TransparentStatic(ctx *context.Context){
+	orpath := ctx.Request.URL.Path
+	beego.Debug("request url: ",orpath)
+
+	if strings.Index(orpath,"api") >= 0{
+		return
+	}
+
+	http.ServeFile(ctx.ResponseWriter,ctx.Request,"static/html/"+ctx.Request.URL.Path)
+}
 func main() {
+	ignoreStaticPath()
 	beego.Run()
 }
 
